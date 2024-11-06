8bd9b97ef7549b225bad37961a412791893c76c4
nicoghaza <114562908+nicoghaza@users.noreply.github.com>
2024-11-06 18:43:20 +0700

    Add files via upload

---
+++ JS Function/index.js
@@ -0,0 +1,47 @@
+function printHeart() {
+    console.log("<3");
+}
+
+printHeart();
+
+
+
+function multiply(a, b) {
+    return a * b;
+}
+
+// Contoh penggunaan:
+console.log(multiply(2, 3)); // 6
+console.log(multiply(9, 9)); // 81
+console.log(multiply(5, 4)); // 20
+
+
+
+let animal = "Giant Pacific Octopus";
+
+function observe() {
+    let animal = "Pajama Squid";
+    console.log(animal);
+}
+
+observe();
+
+
+
+let deadlyAnimal = "Blue-Ringed Octopus";
+
+function handleAnimal() { 
+    let deadlyAnimal = "Scorpionfish"; 
+    console.log(deadlyAnimal); 
+} 
+
+handleAnimal(); 
+console.log(deadlyAnimal);
+
+
+
+const sayHello = (name) => `Hello ${name}!`;
+
+// Contoh penggunaan:
+console.log(sayHello("Hagrid")); // "Hello Hagrid!"
+console.log(sayHello("Luna"));   // "Hello Luna!"
